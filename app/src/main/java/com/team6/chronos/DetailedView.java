041c638181732a1f8bd24dffac5a174012b6060e
axxes8 <axxes8@gmail.com>
2019-06-12 09:58:39 -0600

    modified detailedView activity: removed floating button from view

--- app/src/main/java/com/team6/chronos/DetailedView.java
+++ app/src/main/java/com/team6/chronos/DetailedView.java
@@ -1,11 +1,9 @@
 package com.team6.chronos;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+
 
 public class DetailedView extends AppCompatActivity {
 
@@ -16,13 +14,5 @@ public class DetailedView extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
