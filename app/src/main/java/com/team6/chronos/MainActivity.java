154224bac2f625733e9c77b8e7adad9948e34531
Sandro <barrenwuffett.cpp@gmail.com>
2019-06-13 21:15:05 -0600

    I CHANGED SOMETHING - PLAYING AROUND W BUTTONS

--- app/src/main/java/com/team6/chronos/MainActivity.java
+++ app/src/main/java/com/team6/chronos/MainActivity.java
@@ -1,13 +1,33 @@
 package com.team6.chronos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.button2);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLogin();
+            }
+        });
+
+    }
+
+
+    public void startLogin() {
+        Intent intent = new Intent(this, LoginView.class);
+        startActivity(intent);
     }
 }
