01065e71d3ef3094cf44930df7c9da388b63499a
Taylor <axxes8@gmail.com>
2019-06-19 21:02:25 -0600

    Taylor - Added basic login screen, and start animation

--- app/src/main/java/com/team6/chronos/MainActivity.java
+++ app/src/main/java/com/team6/chronos/MainActivity.java
@@ -1,33 +1,33 @@
 package com.team6.chronos;
 
-import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button loginButton;
+
+    RelativeLayout relLay1, relLay2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            relLay1.setVisibility(View.VISIBLE);
+            relLay2.setVisibility(View.VISIBLE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginButton = findViewById(R.id.button2);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startLogin();
-            }
-        });
+        relLay1=(RelativeLayout) findViewById(R.id.relLay1);
+        relLay2=(RelativeLayout) findViewById(R.id.relLay2);
 
-    }
-
-
-    public void startLogin() {
-        Intent intent = new Intent(this, LoginView.class);
-        startActivity(intent);
+        handler.postDelayed(runnable, 2000); //timer for splash screen
     }
 }
