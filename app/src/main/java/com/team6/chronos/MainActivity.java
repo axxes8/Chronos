6223519f05acdc5331b4362ad8578d26140cd2e2
Taylor <axxes8@gmail.com>
2019-07-01 20:41:15 -0600

    Taylor - Created Signup Page with firebase authentication.

--- app/src/main/java/com/team6/chronos/MainActivity.java
+++ app/src/main/java/com/team6/chronos/MainActivity.java
@@ -1,13 +1,18 @@
 package com.team6.chronos;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button SignupNow;
 
     RelativeLayout relLay1, relLay2;
 
@@ -29,5 +34,18 @@ public class MainActivity extends AppCompatActivity {
         relLay2=(RelativeLayout) findViewById(R.id.relLay2);
 
         handler.postDelayed(runnable, 2000); //timer for splash screen
+
+        SignupNow = findViewById(R.id.btnSignupNow);
+        SignupNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignupActivity();
+            }
+        });
+    }
+
+    public void openSignupActivity(){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/team6/chronos/SignupActivity.java
@@ -0,0 +1,86 @@
+package com.team6.chronos;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class SignupActivity extends AppCompatActivity{
+
+    //Firebase stuff
+    private FirebaseAuth firebaseAuth;
+
+
+    EditText editEmail, editPassword, confPassword;
+    Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        confPassword = findViewById(R.id.confPassword);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editEmail.getText().toString().trim();
+                String password = editPassword.getText().toString().trim();
+                String confirmPassword = confPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(SignupActivity.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(SignupActivity.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(confirmPassword)){
+                    Toast.makeText(SignupActivity.this, "Please Confirm Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(password.equals(confirmPassword)){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        startActivity(new Intent(getApplicationContext(),QuickView.class));
+                                        Toast.makeText(SignupActivity.this, "Registration Complete", Toast.LENGTH_SHORT);
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(SignupActivity.this, "Authentification Failed", Toast.LENGTH_SHORT);
+
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+    }
+
+}
