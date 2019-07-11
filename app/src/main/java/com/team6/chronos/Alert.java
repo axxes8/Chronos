8fe0b8682c307fb0e2349a2b342cd93322ded1ad
Teancum007 <icblamires@gmail.com>
2019-07-10 21:23:36 -0600

    Added comments.

--- app/src/main/java/com/team6/chronos/Alert.java
+++ app/src/main/java/com/team6/chronos/Alert.java
@@ -2,11 +2,13 @@ package com.team6.chronos;
 
 public class Alert {
 
+    // The variables needed for our alerts to work.
     String name;
     String service;
     String date;
     String time;
 
+    // Alert class constructor.
     public void Alert(String n, String s, String d, String t) {
 
         name = n;
--- app/src/main/java/com/team6/chronos/QuickView.java
+++ app/src/main/java/com/team6/chronos/QuickView.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class QuickView extends AppCompatActivity {
+
+    // Variables for RecyclerView.
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
